8d410ed9a345d94e4b081af035da3208564be2d6
emilvn <emilvnielsen@hotmail.com>
2024-02-18 21:20:59 +0100

    styling

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -6,7 +6,7 @@ interface IPageLayoutProps extends PropsWithChildren {
     title?: string;
 }
 
-function PageLayout({ children, title = "Hogwarts Admin" }: IPageLayoutProps) {
+function PageLayout({ children, title = "Hogwarts Student Administration" }: IPageLayoutProps) {
     const [house, setHouse] = useState<House>(House.GRYFFINDOR);
 
     const cycleHouse = () => {
@@ -23,7 +23,7 @@ function PageLayout({ children, title = "Hogwarts Admin" }: IPageLayoutProps) {
         [House.SLYTHERIN]: "bg-green-950 text-slate-300"
     };
     return (<>
-            <div className={`${colors[house]} text-4xl p-4 font-semibold flex gap-2 select-none fixed w-full top-0`}>
+            <div className={`${colors[house]} text-4xl p-4 font-thin font-serif flex gap-2 select-none fixed w-full top-0`}>
                 <img src={houseIcons[house]} alt={house} className="w-12" onClick={cycleHouse} />
                 {title}
             </div>
@@ -32,46 +32,51 @@ function PageLayout({ children, title = "Hogwarts Admin" }: IPageLayoutProps) {
 
                 <div
                     className="fixed opacity-5 min-h-screen pointer-events-none flex justify-center items-center">
-                    <img src={houseIcons[house]} alt={house} className=""/>
+                    <img src={houseIcons[house]} alt={house} className="max-w-[35vw]" />
                 </div>
             </main>
-            <Footer />
+            <Footer house={house} />
             <Toaster position={"bottom-center"} />
         </>
     );
 }
 
-function Footer() {
+function Footer({ house }: { house: House }) {
     return (
-        <div className="flex flex-row p-2 gap-2 text-gray-500 text-xs justify-center">
-            <div className="flex-col">
+        <div
+            className="flex flex-row p-2 gap-2 text-gray-500 bg-white text-xs text-opacity-25 justify-center fixed bottom-0 w-full">
+            {house === House.GRYFFINDOR &&
                 <div>
                     Gryffindor Crest by Whitney Rosenberg from <a
                     href="https://thenounproject.com/browse/icons/term/gryffindor-crest/" target="_blank"
                     title="Gryffindor Crest Icons">Noun Project</a> (CC BY 3.0)
                 </div>
+            }
+            {house === House.HUFFLEPUFF &&
                 <div>
                     Hufflepuff Crest by Whitney Rosenberg from <a
                     href="https://thenounproject.com/browse/icons/term/hufflepuff-crest/" target="_blank"
                     title="Hufflepuff Crest Icons">Noun Project</a> (CC BY 3.0)
                 </div>
+            }
+            {house === House.SLYTHERIN &&
                 <div>
                     Slytherin Crest by Whitney Rosenberg from <a
                     href="https://thenounproject.com/browse/icons/term/slytherin-crest/" target="_blank"
                     title="Slytherin Crest Icons">Noun Project</a> (CC BY 3.0)
                 </div>
-            </div>
-            <div className="flex-col">
+            }
+            {house === House.RAVENCLAW &&
                 <div>
                     Ravenclaw Crest by Whitney Rosenberg from <a
                     href="https://thenounproject.com/browse/icons/term/ravenclaw-crest/" target="_blank"
                     title="Ravenclaw Crest Icons">Noun Project</a> (CC BY 3.0)
                 </div>
-                <div>
-                    Hogwarts shield by Locad from <a
-                    href="https://thenounproject.com/browse/icons/term/hogwarts-shield/" target="_blank"
-                    title="Hogwarts shield Icons">Noun Project</a> (CC BY 3.0)
-                </div>
+            }
+            <div>
+                Hogwarts shield by Locad from <a
+                href="https://thenounproject.com/browse/icons/term/hogwarts-shield/" target="_blank"
+                title="Hogwarts shield Icons">Noun Project</a> (CC BY 3.0)
             </div>
         </div>
     );
